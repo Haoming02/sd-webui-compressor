c97dd172ed35358e2ff32dbda5795d4214852efc
Haoming <hmstudy02@gmail.com>
2025-03-28 15:49:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# SD Webui Compressor
+This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), which adds a new Tab that lets you save an UNet in **fp8** precision to save some storage space.
+
+> Compatible with [Forge](https://github.com/lllyasviel/stable-diffusion-webui-forge)
+
+> Only tested with **SD1** and **SDXL** checkpoints
+
+### Examples
+
+<p align="center"><img src="./SD1.jpg"></p>
+
+- **realisticVision-v5.1**
+    - **fp16:** 1.98 GB
+    - **fp8:** 1.18 GB
+
+<p align="center"><img src="./SDXL.jpg"></p>
+
+- **wai-SHUFFLE-NOOB-vPred-v2.0**
+    - **fp16:** 6.46 GB
+    - **fp8:** 4.07 GB
+
+<hr>
+
+> [!Note]
+> As shown, the results will be different; but the quality is more or less the same
---
+++ scripts/compressor.py
@@ -0,0 +1,62 @@
+import os.path
+
+import gradio as gr
+import torch
+from modules.script_callbacks import on_ui_tabs
+from safetensors.torch import load_file, save_file
+
+MODELS: list[str] = None
+
+
+def convert_to_fp8(idx: int):
+    path: str = MODELS[idx]
+    if not path.endswith(".safetensors"):
+        raise gr.Error(f'"{path}" is not safetensors')
+
+    output = path.replace(".safetensors", "-fp8.safetensors")
+    if os.path.isfile(output):
+        raise gr.Error(f'"{output}" already exists...')
+
+    print("loading...")
+    sd = load_file(path, device="cuda")
+    sd_fp8 = {}
+    keys_fp16 = []
+
+    print("parsing...")
+    for key, weight in sd.items():
+        if key.startswith("model.diffusion_model"):
+            sd_fp8[key] = weight.to(torch.float8_e4m3fn)
+        elif weight.dtype != torch.float16:
+            sd_fp8[key] = weight.to(torch.float16)
+            keys_fp16.append(key)
+        else:
+            sd_fp8[key] = weight
+
+    print("\tconverted model.diffusion_model to", torch.float8_e4m3fn)
+    if keys_fp16:
+        print(f"\tconverted [{', '.join(keys_fp16)}] to", torch.float16)
+
+    print("saving...")
+    save_file(sd_fp8, output)
+
+    gr.Info("Done!")
+
+
+def editor_ui():
+    from modules import sd_models
+
+    global MODELS
+    MODELS = [mdl.filename for mdl in sd_models.checkpoints_list.values()]
+    models = [os.path.basename(mdl) for mdl in MODELS]
+
+    with gr.Blocks() as FP8_EDITOR:
+        with gr.Row():
+            target = gr.Dropdown(value=models[0], choices=models, type="index")
+            button = gr.Button(value="Convert", variant="primary")
+
+        button.click(fn=convert_to_fp8, inputs=[target])
+
+    return [(FP8_EDITOR, "Compressor", "sd-webui-fp8")]
+
+
+on_ui_tabs(editor_ui)
